9a9591f85cc56457fd765a1a48d5ddcae360c20f
RautDayanand <dayanandraut2004@gmail.com>
2025-10-02 11:50:45 +0530

    Initial commit

---
+++ OpenAi1/src/main/java/SpringAi/OpenAi1/OpenAi1Application.java
@@ -0,0 +1,13 @@
+package SpringAi.OpenAi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAi1Application.class, args);
+	}
+
+}
---
+++ OpenAi1/src/test/java/SpringAi/OpenAi1/OpenAi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringAi.OpenAi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
